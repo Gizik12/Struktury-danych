495fd8b56aac4f880f5b7a95b2de4489fbc17f84
serpenteno <fiacekk@gmail.com>
2024-03-15 14:20:48 +0100

    Utworzenie klasy ArrayList, zmienne prywatne i pierwsze funkcje.

---
+++ Miniprojekt_1/Public/ArrayList.h
@@ -0,0 +1,92 @@
+#ifndef ARRAY_LIST
+#define ARRAY_LIST
+
+template<typename DataType, unsigned int Capacity>
+class ArrayList
+{
+public:
+	// Konstruktor
+	ArrayList();
+	
+	// Destruktor
+	~ArrayList();
+
+	// Dodaje element na końcu tablicy.
+	void Add(DataType Element);
+
+	// Dodaje element na początku tablicy.
+	void AddInFront(DataType Element);
+
+	// Zwraca liczbę elementów w tablicy.
+	int GetSize() const { return m_Size; }
+
+	// Zwraca maksymalną pojemność tablicy.
+	int GetCapacity() const { return m_Capacity; }
+
+	// Zwraca wartość elementu tablicy o podanym indeksie.
+	DataType GetElementOfIndex(int Index) const { return m_Elements[Index]; }
+
+private:
+	unsigned int m_Size;		// Liczba elementów w tablicy.
+	unsigned int  m_Capacity;	// Maksymalna pojemność tablicy.
+	DataType* m_Elements;		// Wskaźnik na pierwszy element tablicy.
+
+	// Dwukrotnie zwiększa pojemność tablicy.
+	void DoubleTheCapacity();
+};
+
+
+template<typename DataType, unsigned int Capacity>
+inline ArrayList<DataType, Capacity>::ArrayList()
+	: m_Size(0), m_Capacity(Capacity), m_Elements(nullptr)
+{
+	m_Elements = new DataType[m_Capacity];
+}
+
+template<typename DataType, unsigned int Capacity>
+inline ArrayList<DataType, Capacity>::~ArrayList()
+{
+	if (m_Elements != nullptr)
+	{
+		delete[] m_Elements;
+	}
+}
+
+template<typename DataType, unsigned int Capacity>
+inline void ArrayList<DataType, Capacity>::Add(DataType Element)
+{
+	if (m_Size == m_Capacity) // Jeśli tablica jest zapełniona.
+	{
+		DoubleTheCapacity(); // Dwukrotnie zwiększa pojemność.
+	}
+	m_Elements[m_Size] = Element;
+	++m_Size;
+}
+
+template<typename DataType, unsigned int Capacity>
+inline void ArrayList<DataType, Capacity>::AddInFront(DataType Element)
+{
+	if (m_Size == m_Capacity) // Jeśli tablica jest zapełniona
+	{
+		DoubleTheCapacity(); // Dwukrotnie zwiększ pojemność.
+	}
+	
+	// TODO: Przesunąć każdy element w tablicy o 1
+	// i wstawić Element do m_Elements[0].
+
+}
+
+template<typename DataType, unsigned int Capacity>
+inline void ArrayList<DataType, Capacity>::DoubleTheCapacity()
+{
+	m_Capacity *= 2;
+	DataType* ResizedArray = new DataType[m_Capacity]; // Utworzenie tablicy pomocniczej.
+	for (int i = 0; i < GetSize(); i++)
+	{
+		ResizedArray[i] = m_Elements[i];
+	}
+	m_Elements = ResizedArray;	// Zastąpienie starej tablicy nową tablicą o dwukronie większej pojemności.
+}
+
+
+#endif
--- Miniprojekt_1/main.cpp
+++ Miniprojekt_1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "ArrayList.h"
 
 using namespace std;
 
