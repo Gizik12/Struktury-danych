6ea03d66765f212dd5485f01f3eb1ba321f8f693
serpenteno <fiacekk@gmail.com>
2024-04-28 00:18:51 +0200

    Menu (losowa generacja danych).

--- Miniprojekt_2/main.cpp
+++ Miniprojekt_2/main.cpp
@@ -10,10 +10,13 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "pl_PL"); // Ustawienie języka polskiego.
-	
-	PriorityQueue* priorityQueue = nullptr; // Wskaźnik na wybrany typ kolejki.
+
+	PriorityQueue* priorityQueue;			// Wskaźnik na wybrany typ kolejki.
 	char priorityQueue_type;				// Wybrany typ kolejki.
-	char priorityQueue_op;					// Wybrana operacja kolejki.
+	char priorityQueue_opt;					// Wybrana operacja kolejki.
+	char dataAmt_type;						// Wybrana ilość danych.
+	int dataAmt;							// Ilość danych do prowadzonego badania.
+	short priorityMultiplier = 5;			// Mnożnik maksymalnej wartości wygenerowanego priorytetu (krotność ilości danych).
 
 	chrono::steady_clock::time_point OperationStart;	// Czas rozpoczęcia operacji.
 	chrono::steady_clock::time_point OperationEnd;		// Czas zakończenia operacji.
@@ -28,6 +31,7 @@ int main()
 			cin >> priorityQueue_type;
 		} while (priorityQueue_type != '1' && priorityQueue_type != '2' && priorityQueue_type != 'q');
 
+		priorityQueue = nullptr;
 		switch (priorityQueue_type)
 		{
 		case '1':
@@ -52,74 +56,133 @@ int main()
 			{
 				system("cls");
 				cout << "Wybierz operację:\n1. insert\n2. extractMax\n3. peek\n4. modifyKey\n5. returnSize\nq. Cofnij\n";
-				cin >> priorityQueue_op;
+				cin >> priorityQueue_opt;
 			} while (priorityQueue_type != '1' && priorityQueue_type != '2' && priorityQueue_type != '3' && priorityQueue_type != '4' && priorityQueue_type != '5' && priorityQueue_type != 'q');
-		
-			switch (priorityQueue_op)
+			
+			if (priorityQueue_opt == 'q')
+			{
+				break;
+			}
+
+			do
+			{
+				system("cls");
+				cout << "Wybierz ilość danych:\n1. 5 000\n2. 8 000\n3. 10 000\n4. 16 000\n5. 20 000\n6. 40 000\n7. 60 000\n8. 100 000\n";
+				cin >> dataAmt_type;
+			} while (dataAmt_type != '1' && dataAmt_type != '2' && dataAmt_type != '3' && dataAmt_type != '4' && dataAmt_type != '5' && dataAmt_type != '6' && dataAmt_type != '7' && dataAmt_type != '8');
+
+			switch (dataAmt_type)
 			{
 			case '1':
-				OperationStart = chrono::steady_clock::now();
-				for (int i = 0; i < 100; i++)
-				{
-					priorityQueue->insert();
-				}
-				OperationEnd = chrono::steady_clock::now();
+				dataAmt = 5000;
 				break;
 				
 			case '2':
-				OperationStart = chrono::steady_clock::now();
-				for (int i = 0; i < 100; i++)
-				{
-					priorityQueue->extractMax();
-				}
-				OperationEnd = chrono::steady_clock::now();
+				dataAmt = 8000;
 				break;
 				
 			case '3':
-				OperationStart = chrono::steady_clock::now();
-				for (int i = 0; i < 100; i++)
-				{
-					priorityQueue->peek();
-				}
-				OperationEnd = chrono::steady_clock::now();
+				dataAmt = 10000;
 				break;
 				
 			case '4':
-				OperationStart = chrono::steady_clock::now();
-				for (int i = 0; i < 100; i++)
-				{
-					priorityQueue->modifyKey();
-				}
-				OperationEnd = chrono::steady_clock::now();
+				dataAmt = 16000;
 				break;
 				
 			case '5':
-				OperationStart = chrono::steady_clock::now();
-				for (int i = 0; i < 100; i++)
-				{
-					priorityQueue->returnSize();
-				}
-				OperationEnd = chrono::steady_clock::now();
+				dataAmt = 20000;
 				break;
 				
-			case 'q':
+			case '6':
+				dataAmt = 40000;
+				break;
+				
+			case '7':
+				dataAmt = 60000;
+				break;
+				
+			case '8':
+				dataAmt = 100000;
 				break;
 
 			default:
 				cout << "Unknown error has occured.";
 				return 1;
 			}
-			if (priorityQueue_op == 'q')
+
+			int maxPriority = dataAmt * priorityMultiplier;
+			switch (priorityQueue_opt)
 			{
+			case '1':
+				OperationStart = chrono::steady_clock::now();
+				for (int i = 0; i < dataAmt; i++)
+				{
+					priorityQueue->insert(GenerateRandomNumber(), GenerateRandomNumber(0, maxPriority));
+				}
+				OperationEnd = chrono::steady_clock::now();
 				break;
+
+			case '2':
+				for (int i = 0; i < dataAmt; i++)
+				{
+					priorityQueue->insert(GenerateRandomNumber(), GenerateRandomNumber(0, maxPriority));
+				}
+				OperationStart = chrono::steady_clock::now();
+				for (int i = 0; i < dataAmt; i++)
+				{
+					priorityQueue->extractMax();
+				}
+				OperationEnd = chrono::steady_clock::now();
+				break;
+
+			case '3':
+				for (int i = 0; i < dataAmt; i++)
+				{
+					priorityQueue->insert(GenerateRandomNumber(), GenerateRandomNumber(0, maxPriority));
+				}
+				OperationStart = chrono::steady_clock::now();
+				for (int i = 0; i < dataAmt; i++)
+				{
+					priorityQueue->peek();
+				}
+				OperationEnd = chrono::steady_clock::now();
+				break;
+
+			case '4':
+				for (int i = 0; i < dataAmt; i++)
+				{
+					priorityQueue->insert(GenerateRandomNumber(), GenerateRandomNumber(0, maxPriority));
+				}
+				OperationStart = chrono::steady_clock::now();
+				for (int i = 0; i < dataAmt; i++)
+				{
+					priorityQueue->modifyKey(GenerateRandomNumber(), GenerateRandomNumber(0, maxPriority));
+				}
+				OperationEnd = chrono::steady_clock::now();
+				break;
+
+			case '5':
+				for (int i = 0; i < dataAmt; i++)
+				{
+					priorityQueue->insert(GenerateRandomNumber(), GenerateRandomNumber(0, maxPriority));
+				}
+				OperationStart = chrono::steady_clock::now();
+				for (int i = 0; i < dataAmt; i++)
+				{
+					priorityQueue->returnSize();
+				}
+				OperationEnd = chrono::steady_clock::now();
+				break;
+
+			default:
+				cout << "Unknown error has occured.";
+				return 1;
 			}
-			else
-			{
-				TotalOperationTime = chrono::duration_cast<chrono::nanoseconds>(OperationEnd - OperationStart);
-				cout << "Całkowity czas wykonywania operacji: " << TotalOperationTime.count() / 100 << " ns\n";
-				system("pause");
-				continue;
-			}
+			delete priorityQueue;
+			TotalOperationTime = chrono::duration_cast<chrono::nanoseconds>(OperationEnd - OperationStart);
+			cout << "Całkowity czas wykonywania operacji: " << TotalOperationTime.count() << " ns\n";
+			system("pause");
+			break;
 		}
 	}
 	return 0;
